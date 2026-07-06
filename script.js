c8b564e309a561a69326d92815f89615b20f1293
castromickael-cpu <castro.mickael@escola.pr.gov.br>
2026-07-06 16:36:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function modoClaro() {
+    document.body.style.backgroundColor = "white";
+    document.body.style.color = "black";
+
+document.getElementById("mensagem").innerText= "piiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii";
+
+}
+
+function modoEscuro() {
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white";
+
+    document.getElementById("mensagem").innerText= "fechar os olhos";}
