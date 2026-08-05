7b75d038692189030333456b8cbcb5433e210f94
castromickael-cpu <castro.mickael@escola.pr.gov.br>
2026-08-05 11:44:30 -0300

    Add files via upload

---
+++ atvgab10/script.js
@@ -0,0 +1,13 @@
+function verificarLogin() {
+let usuario = document.getElementById("usuario").ariaValueMax.trim();
+let senha = document.getElementById("resultado");
+
+if ( usuario ==="" || senha === ""){ 
+    resultado.innerText = 
+
+
+}
+
+
+
+}
